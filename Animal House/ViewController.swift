d30de62fe164d8dcf215b5128641d228fbe889d5
orlov <orlov@iMac.local>
2016-06-10 07:21:49 +0300

    Initial Commit

---
+++ Animal House/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal House
+//
+//  Created by iMac on 10.06.16.
+//  Copyright © 2016 Vasili Orlov House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
