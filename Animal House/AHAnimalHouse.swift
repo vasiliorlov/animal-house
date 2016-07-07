5d1ae91a9324825875b7c0b11eaa2c8a8bd51dd2
orlov <orlov@iMac.local>
2016-07-07 22:17:28 +0300

    without magical record

---
+++ Animal House/AHAnimalHouse.swift
@@ -0,0 +1,13 @@
+//
+//  AHAnimalHouse.swift
+//  Animal House
+//
+//  Created by iMac on 06.07.16.
+//  Copyright © 2016 Vasili Orlov House. All rights reserved.
+//
+
+import UIKit
+
+class AHAnimalHouse: NSObject {
+
+}
